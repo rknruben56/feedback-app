a651679ecd406c0f72782b045207289d9eed1647
Ruben Rodriguez <rrodriguez6@outlook.com>
2021-02-04 22:28:43 -0600

    organized css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../styles/index.css"
 
 const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
